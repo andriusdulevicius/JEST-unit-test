14ea56c874e61b148bba780dcd5b16d115493907
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-28 11:53:42 +0300

    test async fn async/await way

--- functions/promise.test.js
+++ functions/promise.test.js
@@ -3,3 +3,9 @@ it('should contain email', () => {
   expect.assertions(1);
   return getUser().then((data) => expect(data.email).toBe('Shanna@melissa.tv'));
 });
+
+it('should contain email ASYNC', async () => {
+  expect.assertions(1);
+  const data = await getUser();
+  expect(data.email).toBe('Shanna@melissa.tv');
+});
